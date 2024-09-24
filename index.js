1e6f0918a9e04cce9af68872a1a553eb7e67daef
vishalicious213 <vishalicious213@gmail.com>
2024-09-23 19:09:05 -0700

    drafted getNumber() & getOperation(). logging clicked button with them

--- index.js
+++ index.js
@@ -2,12 +2,37 @@ let buttons = document.getElementById("buttons")
 let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
 let operations = ["/", "x", "+", "-", "=", "."]
 
+let num1 = null
+let num2 = null
+let total = null
+
+// ⬇️ EVENT LISTENERS ⬇️
+
 buttons.addEventListener("click", function(e) {
     if (numbers.includes(e.target.id)) {
-        console.log(e.target.id)
+        getNumber(e.target.id)
     }
     
     if (operations.includes(e.target.id)) {
-        console.log(e.target.id)
+        getOperation(e.target.id)
     }
 })
+
+// ⬇️ EVENT HANDLERS ⬇️
+
+function getNumber(num) {
+    console.log(num)
+}
+
+function getOperation(op) {
+    console.log(op)
+}
+
+// ⬇️ RENDER APP ⬇️
+
+
+// Get num1
+// Get operator
+// Get num2
+// Display total when = is clicked
+// Handle negative numbers
