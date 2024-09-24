3829da594b8730e39f773b1c398ca8519bbeb345
vishalicious213 <vishalicious213@gmail.com>
2024-09-23 18:39:30 -0700

    logging clicked numbers

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log("Calculator")
+let buttons = document.getElementById("buttons")
+let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+
+buttons.addEventListener("click", function(e) {
+    if (numbers.includes(e.target.id)) {
+        console.log(e.target.id)
+    }
+})
