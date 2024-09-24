da8de555dcb58dc7d02b18bb6fe6251ce55847cd
vishalicious213 <vishalicious213@gmail.com>
2024-09-23 18:50:25 -0700

    logging clicked function buttons

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 let buttons = document.getElementById("buttons")
 let numbers = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"]
+let operations = ["/", "x", "+", "-", "=", "."]
 
 buttons.addEventListener("click", function(e) {
     if (numbers.includes(e.target.id)) {
+        console.log(e.target.id)
+    }
+    
+    if (operations.includes(e.target.id)) {
         console.log(e.target.id)
     }
 })
