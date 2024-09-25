fce7185c0229763599ac70b5f4e3c9261fe82106
vishalicious213 <vishalicious213@gmail.com>
2024-09-25 13:58:44 -0700

    initializing event listeners

--- index.js
+++ index.js
@@ -5,5 +5,13 @@ class Calculator {
         this.display = document.getElementById(displayID)
         this.buttons = document.getElementById(buttonsID)
         this.clearBtn = document.getElementById(clearBtnID)
+
+        this.initialize()
+    }
+
+    // ⬇️ EVENT LISTENERS ⬇️
+    initialize() {
+        this.buttons.addEventListener("click", (e) => this.handleButtonClick(e))
+        this.clearBtn.addEventListener("click", () => this.clearDisplay())
     }
 }
